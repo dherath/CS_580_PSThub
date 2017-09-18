34e8145577b93a984938f9a059ee90cb56500e3e
dinal herath <dinal.bing@gmail.com>
2017-09-18 09:38:28 -0400

    program2: part 1- testing with print statements

--- programs/program2/mylib.h
+++ programs/program2/mylib.h
@@ -1,29 +1,14 @@
+#include <stdio.h>
 
 int myStrStr(char *haystack,char *needle,char *buffer){
-  int size =0;
-  for(char *temp=needle;*temp!='\0';temp++){
-    size++;
-  }
-  
-  while(*haystack != *needle){
-    haystack++;
-    if(*haystack=='\0'){
-      return 0;
-    }
-  }
-
-  for(char *i=haystack;*i!='\0';i++){
-    char *j = needle;
-    char *k = i;
-    int count = 0;
-    while (*j==*k && count<size){
-      j++;
-      *(buffer+count)=*k;
-      k++;
-      count++;
-    }
-    if(count==size-1){
-      return 1;
+  for(char * i = haystack;*i!='\0';i++){
+    char * temp1 = i;
+    for(char *j = needle;*j!='\0';j++){
+      if(*temp1 == *j){
+	printf("%c",*temp1);
+	temp1++;
+	j++;
+      }
     }
   }
   return 0;
