b24dc28a97abc85a4911366aabed973c30ad28b9
dinal herath <dinal.bing@gmail.com>
2017-09-18 09:47:37 -0400

    program 2: part 1- working for 'choc' case printf()

--- programs/program2/mylib.h
+++ programs/program2/mylib.h
@@ -3,12 +3,22 @@
 int myStrStr(char *haystack,char *needle,char *buffer){
   for(char * i = haystack;*i!='\0';i++){
     char * temp1 = i;
-    for(char *j = needle;*j!='\0';j++){
-      if(*temp1 == *j){
-	printf("%c",*temp1);
+    /*for(char *j = needle;*(j+1)!='\0';j++){
+      if(*j== *temp1){
+	printf("%c\n",*temp1);
 	temp1++;
 	j++;
       }
+      }*/
+    char * temp2 = needle;
+    while(*(temp2+1)!='\0'){
+      if(*temp1==*temp2){
+	printf("%c\n",*temp1);
+	temp1++;
+	temp2++;
+      }else{
+	break;
+      }      
     }
   }
   return 0;
