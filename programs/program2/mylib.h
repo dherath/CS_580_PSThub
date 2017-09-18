6fdd9878d6a7808c775961b56e3582fa9a6a7b7f
dinal herath <dinal.bing@gmail.com>
2017-09-18 10:21:33 -0400

    program 2: part 1-> removed commented lines

--- programs/program2/mylib.h
+++ programs/program2/mylib.h
@@ -1,29 +1,19 @@
 #include <stdio.h>
 
 int myStrStr(char *haystack,char *needle,char *buffer){
-  /* int needleSize =0;
-  char *bufferIndex = buffer;
-  for(char *tempNeedle=needle;*tempNeedle!='\0';tempNeedle++){
-    needleSize++;
-  }
-  printf("needle size =%d\n",needleSize);
-  */
+
   char *bufferIndex=buffer;
   
   for(char * i = haystack;*i!='\0';i++){
     char * temp1 = i;
     char * temp2 = needle;
     buffer = bufferIndex;
-    //    int tempSize =0;
     int count = 0;
     while(*temp2!='\0'){
-      if(*temp1==*temp2)/* && count<needleSize)*/{
-	//*(buffer+count)=*temp1;
+      if(*temp1==*temp2){
 	*buffer=*temp1;
-	printf("charachter = %c buffer value = %c\n",*temp1,*buffer);
 	temp1++;
 	temp2++;
-	//count++;
 	buffer++;
 	if(*temp2=='\0'){
 	  *buffer = '\0';
