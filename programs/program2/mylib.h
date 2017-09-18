8168686cbf531afda756f61639213c905d75a047
dinal herath <dinal.bing@gmail.com>
2017-09-18 10:20:39 -0400

    program2: pasrt 1- pointer strings-> most tests passed

--- programs/program2/mylib.h
+++ programs/program2/mylib.h
@@ -1,25 +1,39 @@
 #include <stdio.h>
 
 int myStrStr(char *haystack,char *needle,char *buffer){
+  /* int needleSize =0;
+  char *bufferIndex = buffer;
+  for(char *tempNeedle=needle;*tempNeedle!='\0';tempNeedle++){
+    needleSize++;
+  }
+  printf("needle size =%d\n",needleSize);
+  */
+  char *bufferIndex=buffer;
+  
   for(char * i = haystack;*i!='\0';i++){
     char * temp1 = i;
-    /*for(char *j = needle;*(j+1)!='\0';j++){
-      if(*j== *temp1){
-	printf("%c\n",*temp1);
-	temp1++;
-	j++;
-      }
-      }*/
     char * temp2 = needle;
-    while(*(temp2+1)!='\0'){
-      if(*temp1==*temp2){
-	printf("%c\n",*temp1);
+    buffer = bufferIndex;
+    //    int tempSize =0;
+    int count = 0;
+    while(*temp2!='\0'){
+      if(*temp1==*temp2)/* && count<needleSize)*/{
+	//*(buffer+count)=*temp1;
+	*buffer=*temp1;
+	printf("charachter = %c buffer value = %c\n",*temp1,*buffer);
 	temp1++;
 	temp2++;
+	//count++;
+	buffer++;
+	if(*temp2=='\0'){
+	  *buffer = '\0';
+	  return 1;
+	}
       }else{
 	break;
       }      
     }
   }
+  
   return 0;
 }
