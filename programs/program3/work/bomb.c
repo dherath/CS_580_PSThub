ef042e7a6bc318b203875946e8274a36028e238d
dinal herath <dinal.bing@gmail.com>
2017-10-06 14:15:21 -0400

    fixed issues with memory leak for bomb.c

--- programs/program3/work/bomb.c
+++ programs/program3/work/bomb.c
@@ -5,34 +5,38 @@ void phase(int num);
 char * generatePasscode(int num);
 
 void phase_1(){
-	char * pass = generatePasscode(1);
-	free(pass);
-	phase(1);
-	//free(pass);
+  char * pass = generatePasscode(1);
+  free(pass);
+  phase(1);
 }
+
 void phase_2(){
-	char * pass = generatePasscode(2);
-	phase(2);
-	free(pass);
+  char * pass = generatePasscode(2);
+  free(pass);
+  phase(2);
 }
+
 void phase_3(){
-	char * pass = generatePasscode(3);
-	phase(3);
-	free(pass);
+  char * pass = generatePasscode(3);
+  free(pass);
+  phase(3);
 }
+
 void phase_4(){
-	char * pass = generatePasscode(4);
-	phase(4);
-	free(pass);
+  char * pass = generatePasscode(4);
+  free(pass);
+  phase(4);
 }
+
 void phase_5(){
-	char * pass = generatePasscode(5);
-	phase(5);
-	free(pass);
+  char * pass = generatePasscode(5);
+  free(pass);
+  phase(5);
 }
+
 void phase_6(){
-	char * pass = generatePasscode(6);
-	phase(6);
-	createKey();
-	free(pass);
+  char * pass = generatePasscode(6);
+  free(pass);
+  phase(6);
+  createKey();
 }
