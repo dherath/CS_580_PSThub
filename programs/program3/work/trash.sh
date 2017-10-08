3d455965e078bab77283b7668b8f8d918de998ac
dinal herath <dinal.bing@gmail.com>
2017-10-07 20:03:36 -0400

    program 3 : part 3 added funciton to move file to Trash

--- programs/program3/work/trash.sh
+++ programs/program3/work/trash.sh
@@ -5,19 +5,32 @@
 
 LIST=$@
 
-#------Making TRASH directory if needed-----------
-TEMP=0
-ITEMS=`ls`
-for THINGS in $ITEMS ; do
-    if [ -d $THINGS ] && [ "$THINGS" = 'TRASH' ]; then
-	TEMP=1
-    fi
-done
+if [ $# -gt 0 ];then
+    #------Making TRASH directory if needed-----------
+    TEMP=0
+    ITEMS=`ls`
+    for THINGS in $ITEMS ; do
+	if [ -d $THINGS ] && [ "$THINGS" = 'TRASH' ]; then
+	    TEMP=1
+	fi
+    done
 
-if [ $TEMP -eq 0 ]; then
-    mkdir $PWD/TRASH
+    if [ $TEMP -eq 0 ]; then
+	mkdir $PWD/TRASH
+    fi
+    #----------moving to TRASH-----------------------
+    for FILE in $LIST ; do
+	if [ -f $FILE ]; then
+	    mv $FILE TRASH/
+	else
+	    echo " $FILE is not in directory "
+	fi
+    done   
+    
+    #------------------------------------------------
+else
+    echo "no arguments given "
 fi
 
-#------------------------------------------------
 
 
