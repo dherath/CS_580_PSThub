4883ec7302a50f1668fc4214880f1a3fd51569f9
dinal herath <dinal.bing@gmail.com>
2017-11-02 11:28:06 -0400

    Added struct PLayer

---
+++ programs/program4/work/tournament.h
@@ -0,0 +1,18 @@
+#ifendif TOURNAMENT_H
+
+#define TOURNAMENT_H
+
+typedef struct Player{
+  int offensive;
+  int defensive;
+  int number;
+  char * first;
+  char * last;
+}Player;
+
+
+
+
+
+
+#endif
