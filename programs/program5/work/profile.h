68983707b07e17451a4561354e704f2a6892cfce
dinal herath <dinal.bing@gmail.com>
2017-11-25 20:15:45 -0500

    p5:part c -> profiling start

---
+++ programs/program5/work/profile.h
@@ -0,0 +1,14 @@
+#include "data.h"
+#include "vector.h"
+#include "list.h"
+
+
+#ifndef DH_PROFILE_H
+
+#define DH_PROFILE_H
+
+void profileInsert(Vector * vect, List * list);
+void profileRead(Vector * vect, List * list);
+void profileRemove(Vector * vect, List * list);
+
+#endif
