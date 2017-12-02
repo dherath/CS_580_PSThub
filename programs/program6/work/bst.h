86c1ea524ffd755000508c7c0826a5c7a6123263
dinal herath <dinal.bing@gmail.com>
2017-12-01 20:55:13 -0500

    created bst.h file

--- programs/program6/work/bst.h
+++ programs/program6/work/bst.h
@@ -1,7 +1,5 @@
-#ifndef DH_LIST_H
-#define DH_LIST_H 
-
-#include "data.h"
+#ifndef DH_BST_H
+#define  DH_BST_H 
 
 #pragma pack(1)
 
@@ -9,38 +7,5 @@
 // B no - B00708543
 
 
-/**
- *struct for a Node
- **/
-typedef struct Node{
-  struct Data data;
-  struct Node * next, * prev;
-}Node;
-
-/**
- *struct for a List
- **/
-typedef struct List{
-  int size;
-  struct Node * head, * tail;
-  void (* insert)(struct List * list,int index, Data value);
-  struct Data * (* read)(struct List * list, int index);
-  void (* remove)(struct List * list, int index);
-  void (* delete)(struct List * list);
-}List;
-
-
-//-- functions------------
-
-List * newList();
-void insertList(List * list,int index, Data data);
-Data * readList(List * list, int index);
-void removeList(List * list, int index);
-void deleteList(List * list);
-
-//---- helper functions--------
-
-Node * newNode();
-void freeNode(Node * n);
 
 #endif
